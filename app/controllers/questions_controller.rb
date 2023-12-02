10337882d542520584321f790abbc188dd296fe2
kuruneru <kuruneru.ikkou@gmail.com>
2023-12-02 16:37:02 +0900

    application

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -31,4 +31,26 @@ class QuestionsController < ApplicationController
     def question_params
       params.require(:question).permit(:content, :answer)
     end
+
+    before_action :set_question, only: [:show, :check_answer]
+
+    def show
+      @user_answer = ""
+    end
+
+    def check_answer
+      user_answer = params[:user_answer]
+      if user_answer.downcase == @question.answer.downcase
+        flash[:notice] = "正解です！"
+      else
+        flash[:alert] = "不正解です。正解は「#{@question.answer}」です。"
+      end
+      redirect_to action: :show
+    end
+
+    private
+
+    def set_question
+      @question = Question.order("RANDOM()").first
+    end
 end
